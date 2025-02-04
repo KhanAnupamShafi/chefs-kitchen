b24a7873ec8001947a75c6803578b34aadd542d8
KhanAnupamShafi <deep71.bd@gmail.com>
2025-02-04 14:58:35 +0600

    config typo

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  basePath: isProd ? "/materialpro-nextjs-free" : "",
-  assetPrefix: isProd ? "/materialpro-nextjs-free/" : "/",
+  basePath: isProd ? "/chefs-kitchen" : "",
+  assetPrefix: isProd ? "/chefs-kitchen/" : "/",
   output: "export",
   images: {
     unoptimized: true,
