78a865a80540dbc9a9f10c9e4e05cffe5da000da
KhanAnupamShafi <deep71.bd@gmail.com>
2025-02-04 13:27:32 +0600

    image fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   output: "export",
+  basePath: "/chefs-kitchen",
 };
 
 export default nextConfig;
