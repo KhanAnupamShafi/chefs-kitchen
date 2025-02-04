9e9e20402ca2b3fb1aa8a35e85793d73dc629b2d
KhanAnupamShafi <deep71.bd@gmail.com>
2025-02-04 13:14:06 +0600

    config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? "/chefs-kitchen/" : "",
-  basePath: process.env.DEPLOYED_GITHUB_PATH || "",
   output: "export",
 };
 
