f0745549d32b27e2b39e2189accf74d39c726117
KhanAnupamShafi <deep71.bd@gmail.com>
2025-02-04 12:34:49 +0600

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/chefs-kitchen" : "",
+  assetPrefix: isProd ? "/chefs-kitchen/" : "",
+};
 
 export default nextConfig;
