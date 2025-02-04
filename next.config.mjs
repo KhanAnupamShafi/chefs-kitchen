21f7920c118025ad239ebf34a09a21be80255992
KhanAnupamShafi <deep71.bd@gmail.com>
2025-02-04 14:54:20 +0600

    add env

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
+  basePath: isProd ? "/materialpro-nextjs-free" : "",
+  assetPrefix: isProd ? "/materialpro-nextjs-free/" : "/",
+  output: "export",
   images: {
     unoptimized: true,
   },
-  output: "export",
-  basePath: "/chefs-kitchen",
-  assetPrefix: "/chefs-kitchen/",
 };
 
 export default nextConfig;
